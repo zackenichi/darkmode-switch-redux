b8f04f2f6b18aea692860467b780334998b93de6
zackenichi <thekenichi26@gmail.com>
2023-10-17 16:44:20 +0800

    app header structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { AppBar, Box, Grid, Toolbar, Typography } from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box sx={{ flexGrow: 1, alignItems: 'center' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container alignItems="center">
+            <Grid item xs={8}>
+              <Box display="flex" alignItems="center">
+                <Brightness4Icon />
+                <Typography variant="h3">DarkMode Switch</Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={4} textAlign="right">
+              <Typography>Switch</Typography>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export { Header };
